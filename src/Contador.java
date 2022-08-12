5a0d84cf141ab4bf9a65343a1acb097972ad58b6
GabrielSocon <Azevedo13>
2022-08-12 09:18:18 -0300

    Contador

---
+++ src/Contador.java
@@ -0,0 +1,14 @@
+public class Contador {
+    int contador;
+
+    public int getContador() {
+        return contador;
+    }
+
+    public Contador(){
+    }
+
+    public void contar(){
+        this.contador = contador + 1;
+    }
+}
