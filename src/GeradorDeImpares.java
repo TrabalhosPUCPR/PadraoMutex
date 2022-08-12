bf2eeeb9ae8f875eb4861d61be76c49a288f01f0
KnightLeo <knightleo602@gmail.com>
2022-08-12 09:38:26 -0300

    erros arrumados

--- src/GeradorDeImpares.java
+++ src/GeradorDeImpares.java
@@ -1,28 +1,29 @@
-package com.company;
-
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class GeradorDeImpares extends Thread{
-    private int[] Impar;
+    private int[] chave;
     private Semaphore mutex;
+    private int qntd_impares;
+    private int contGlobal;
 
-
-    public GeradorDeImpares(int[] Impar, Semaphore mutex) {
-        this.Impar = Impar;
+    public GeradorDeImpares(int[] chave, Semaphore mutex, int contGlobal) {
+        this.chave = chave;
         this.mutex = mutex;
+        this.qntd_impares = 0;
+        this.contGlobal = contGlobal;
     }
     public void run() {
         try {
-            Random r = new Random();
-            Thread.sleep(r.nextInt(1000,3000));
-            mutex.acquire();
-            int Impar = r.nextInt(1,9);
-            if (Impar % 2 == 0)Impar++;
-            for(int i = 0; i < array.lenght; i++){
-
+            while(this.contGlobal < 100){
+                Random r = new Random();
+                // pra pega numero aleatorio entre 2 numeros: (maximo - min) + min
+                Thread.sleep(r.nextInt(2000) + 1000);
+                mutex.acquire();
+                int Impar = r.nextInt(8) + 1;
+                if (Impar % 2 == 0) Impar++;
+                mutex.release();
             }
-            mutex.release();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/GeradorDePares.java
+++ src/GeradorDePares.java
@@ -1,32 +1,29 @@
-package com.company;
-
-import java.sql.Array;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class GeradorDePares extends Thread{
-
-    private int[] Par;
+    private int[] chave;
     private Semaphore mutex;
+    private int qntd_pares;
+    private int contGlobal;
 
-
-
-    public GeradorDePares(int[] Par, Semaphore mutex) {
-        this.Par = Par;
+    public GeradorDePares(int[] chave, Semaphore mutex, int contGlobal) {
+        this.chave = chave;
         this.mutex = mutex;
+        this.qntd_pares = 0;
+        this.contGlobal = contGlobal;
     }
-
     public void run() {
         try {
-            Random r = new Random();
-            Thread.sleep(r.nextInt(1000,3000));
-            mutex.acquire();
-            int Par = r.nextInt(0,8);
-            if (Par % 2 == 1)Par++;
-            for(int i = 0; i < array.length; i++){
-
+            while(this.contGlobal < 100){
+                Random r = new Random();
+                // pra pega numero aleatorio entre 2 numeros: (maximo - min) + min
+                Thread.sleep(r.nextInt(2000) + 1000);
+                mutex.acquire();
+                int par = r.nextInt(8) + 1;
+                if (par % 2 == 0) par++;
+                mutex.release();
             }
-            mutex.release();
         } catch (Exception e) {
             e.printStackTrace();
         }
